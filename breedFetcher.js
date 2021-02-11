8995778fe9e807d57517aeaa0921c2258097fa1b
OdeliaFink <odelia.finkel@gmail.com>
2021-02-11 20:29:35 +0000

    changed the template literal in request url for breedName and exported into index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,32 @@
 const request = require('request');
 const fs = require('fs')
-const command = process.argv.slice(2)
-const path = command[0]
 
 //this is a get request
-request(`https://api.thecatapi.com/v1/images/search?breed_ids=${path}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName , callback) {
+request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName}`, (error, response, body) => {
   if (error || response.statusCode !== 200) {
     console.log('error:', error);
     console.log('status code:', response.statusCode)
     return;
   }
-
-  const data = JSON.parse(body);
-  console.log(data)
   // console.log(typeof data) returns object
-
+  const data = JSON.parse(body);
+  console.log("this is body:", body)
   if (data.length === 0) {
     console.log("Breed not found");
     return;
   }
+  if (error) {
+    callback(error, null);
+  } else {
+    const data = JSON.parse(body);
+  }
+  if (!data[0]) {
+    callback(null, "Breed not found")
+  } else {
+    callback(null, data[0].description)
+  }
 })
+}
 
+module.exports = { fetchBreedDescription };
