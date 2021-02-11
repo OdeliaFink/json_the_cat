9ead37f74a2f7618e84f620a5f87e383144a8303
OdeliaFink <odelia.finkel@gmail.com>
2021-02-11 21:53:28 +0000

    replaced console.log with callback for error

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 //this is a get request
 const fetchBreedDescription = function(breedName , callback) {
-  request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error || response.statusCode !== 200) {
       console.log('error:', error);
       console.log('status code:', response.statusCode);
@@ -10,19 +10,23 @@ const fetchBreedDescription = function(breedName , callback) {
     }
 
     const data = JSON.parse(body);
-    console.log("this is body:", body);
     if (data.length === 0) {
-      console.log("Breed not found");
+      console.log("1")
+      callback("Breed not found", null);
       return;
     }
     if (error) {
+      console.log("2")
       callback(error, null);
     } else {
-      // const data = JSON.parse(body);
+      const data = JSON.parse(body);
     }
     if (!data[0]) {
-      callback(null, "Breed not found");
+      console.log("3")
+      callback("Breed not found", null);
     } else {
+      // console.log("this is body:", body, data);
+      console.log("4")
       callback(null, data[0].description);
     }
   });
