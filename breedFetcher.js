fa1760b4dcd8e59aa86eea55443a0e198ffa9705
OdeliaFink <odelia.finkel@gmail.com>
2021-02-11 19:25:39 +0000

    made request to get breed name and description

---
+++ breedFetcher.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const fs = require('fs')
+const command = process.argv.slice(2)
+const path = command[0]
+
+//this is a get request
+const fetchBreedDescription = function() {
+request(`https://api.thecatapi.com/v1/images/search?breed_ids=${path}`, (error, response, body) => {
+  if (error || response.statusCode !== 200) {
+    console.log('error:', error);
+    console.log('status code:', response.statusCode)
+    return 
+  }
+
+  const data = JSON.parse(body);
+  console.log(data)
+  // console.log(typeof data) returns object
+
+  if (data.length === 0) {
+    console.log("Breed not found");
+    return;
+  }
+})
+}
+
+
