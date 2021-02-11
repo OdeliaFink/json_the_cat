82350d1d5f3d0ee558285ea885dbac0902d9f06b
OdeliaFink <odelia.finkel@gmail.com>
2021-02-11 20:36:30 +0000

    linted version

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,31 @@
 const request = require('request');
-const fs = require('fs')
 
 //this is a get request
 const fetchBreedDescription = function(breedName , callback) {
-request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName}`, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.log('error:', error);
-    console.log('status code:', response.statusCode)
-    return;
-  }
-  // console.log(typeof data) returns object
-  const data = JSON.parse(body);
-  console.log("this is body:", body)
-  if (data.length === 0) {
-    console.log("Breed not found");
-    return;
-  }
-  if (error) {
-    callback(error, null);
-  } else {
+  request(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedName}`, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.log('error:', error);
+      console.log('status code:', response.statusCode);
+      return;
+    }
+
     const data = JSON.parse(body);
-  }
-  if (!data[0]) {
-    callback(null, "Breed not found")
-  } else {
-    callback(null, data[0].description)
-  }
-})
-}
+    console.log("this is body:", body);
+    if (data.length === 0) {
+      console.log("Breed not found");
+      return;
+    }
+    if (error) {
+      callback(error, null);
+    } else {
+      // const data = JSON.parse(body);
+    }
+    if (!data[0]) {
+      callback(null, "Breed not found");
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
 
 module.exports = { fetchBreedDescription };
